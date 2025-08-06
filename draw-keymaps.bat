b8809872d6632b6e5882201d42fd2cbf69a7c3bc
theor <theo.richart@gmail.com>
2025-08-06 11:14:19 -0400

    wip zmk helpers

--- draw-keymaps.bat
+++ draw-keymaps.bat
@@ -1,2 +1,2 @@
-keymap draw .\config\keymap.yaml -o .\corne-keymap.svg
-keymap draw .\lily-config\keymap.yaml -o .\lily58-keymap.svg
+keymap -c config.yaml draw .\config\keymap.yaml -o .\corne-keymap.svg
+keymap -c config.yaml draw .\lily-config\keymap.yaml -o .\lily58-keymap.svg
--- gen-keymaps.bat
+++ gen-keymaps.bat
@@ -1,2 +1,2 @@
-keymap parse -c 12 -z .\config\corne.keymap -o .\config\keymap.yaml
-keymap parse -c 12 -z .\lily-config\lily58.keymap -o .\lily-config\keymap.yaml
+keymap -c config.yaml parse -c 12 -z .\config\corne.keymap -o .\config\keymap.yaml
+keymap -c config.yaml parse -c 12 -z .\lily-config\lily58.keymap -o .\lily-config\keymap.yaml
