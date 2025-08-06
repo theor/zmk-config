85053fc113d950d186f89d6dce4c62d691007ed4
theor <theo.richart@gmail.com>
2025-08-06 10:20:39 -0400

    drawer

---
+++ draw-keymaps.bat
@@ -0,0 +1,2 @@
+keymap draw .\config\keymap.yaml -o .\corne-keymap.svg
+keymap draw .\lily-config\keymap.yaml -o .\lily58-keymap.svg
---
+++ gen-keymaps.bat
@@ -0,0 +1,2 @@
+keymap parse -c 12 -z .\config\corne.keymap -o .\config\keymap.yaml
+keymap parse -c 12 -z .\lily-config\lily58.keymap -o .\lily-config\keymap.yaml
