0aaf5b16a32bf1374aea1084e6c8a86d837d7306
theor <theo.richart@gmail.com>
2025-08-07 11:19:19 -0400

    wip

--- draw-keymaps.bat
+++ draw-keymaps.bat
@@ -1,2 +1,2 @@
-keymap -c config.yaml draw .\config\keymap.yaml -o .\corne-keymap.svg
-keymap -c config.yaml draw .\lily-config\keymap.yaml -o .\lily58-keymap.svg
+keymap -c config.yaml draw .\config\corne.yaml -o .\corne-keymap.svg
+keymap -c config.yaml draw .\config\lily58.yaml -o .\lily58-keymap.svg
