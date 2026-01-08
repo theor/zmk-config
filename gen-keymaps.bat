68c2310d74640a40f30caf56f6e16b845a97aea8
theor <theo.richart@gmail.com>
2026-01-08 12:56:18 -0500

    dongle

--- gen-keymaps.bat
+++ gen-keymaps.bat
@@ -1,2 +1,2 @@
-keymap -c config.yaml parse -c 12 -z .\config\corne.keymap -o .\config\corne.yaml
+@REM keymap -c config.yaml parse -c 12 -z .\config\corne.keymap -o .\config\corne.yaml
 keymap -c config.yaml parse -c 12 -z .\config\lily58.keymap -o .\config\lily58.yaml
--- keymaps.bat
+++ keymaps.bat
@@ -1,2 +1,3 @@
-start cmd /c  gen-keymap.bat
-start cmd /c  draw-keymap.bat
+keymap -c config.yaml parse -c 12 -z .\config\lily58.keymap -o .\config\lily58.yaml
+
+keymap -c config.yaml draw .\config\lily58.yaml -o .\lily58-keymap.svg
